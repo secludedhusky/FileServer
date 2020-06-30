4bf09bbde25c2bf9a5d59d01badbe323ebdd26a8
XenoWarrior <admin@projectge.com>
2020-06-30 15:10:45 +0100

    Initial version

---
+++ index.js
@@ -0,0 +1,31 @@
+// Modules
+const express = require("express");
+const BodyParser = require('body-parser');
+const ConnectBusboy = require('connect-busboy');
+
+// DotEnv
+require('dotenv').config();
+
+// App Initialisation
+const app = express();
+const router = express.Router();
+const port = process.env.PORT || 3030;
+app.use(BodyParser.urlencoded({ extended: true }));
+app.use(BodyParser.json());
+
+app.use(ConnectBusboy());
+
+// Middleware
+const UploadFile = require("./routes/FileUpload");
+const GetFile = require("./routes/FileGet");
+
+app.use(`${process.env.API_V1}`, new UploadFile().GetRoutes());
+app.use(`${process.env.API_V1}`, new GetFile().GetRoutes());
+
+// Start App
+(async function () {
+    app.use(router);
+    app.listen(port, function () {
+        console.log(`Server is listening on port: ${port}`);
+    });
+})();
---
+++ routes/FileUpload.js
@@ -0,0 +1,102 @@
+const Database = require("../lib/database-manager");
+const database = new Database(process.env.DB_HOST, process.env.DB_USER, process.env.DB_PASS, process.env.DB_DATABASE);
+
+const path = require('path');
+const fs = require('fs-extra');
+const mime = require("mime-types");
+const { v4: uuid } = require("uuid");
+
+class FileUpload {
+
+    constructor() {
+        database.connect()
+            .then((r) => {
+                console.log(`${this.constructor.name}: Connected to database.`);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+
+    /**
+     * Upload File
+     * @param {object} req | Incoming request data
+     * @param {object} res | Handle responses
+     */
+    async uploadFile(req, res) {
+        req.pipe(req.busboy);
+        req.busboy.on('file', function (fieldname, file, filename) {
+            try {
+                console.log("Uploading:", filename);
+
+                let requestId = uuid();
+                
+                let fileExt = path.extname(filename);
+                let newFilename = `${requestId}${fileExt}`;
+                let filePath = `${__dirname}/public/${newFilename}`;
+                let fileMime = mime.lookup(filename);
+
+                let stream = fs.createWriteStream(filePath);
+                file.pipe(stream);
+
+                stream.on('error', (error) => {
+                    console.log(error);
+                    res
+                        .status(500)
+                        .send({
+                            status: 500,
+                            message: `An error occured: ${error.message}`
+                        });
+                })
+
+                stream.on('close', function () {
+                    console.log("Uploaded: ", filename);
+                    
+                    database.insert(process.env.UPLOAD_TABLE_V1, { 
+                        upload_id: requestId, 
+                        upload_path: filePath, 
+                        upload_user: "todo",
+                        upload_filename: filename,
+                        upload_mime: fileMime
+                    })
+                        .then((r) => {
+                            res
+                                .set({ "Content-Type": "application/json" })
+                                .status(200)
+                                .send({
+                                    status: 200,
+                                    data: {
+                                        link: `http://${req.hostname}${process.env.APP_PORT === 80 || process.env.APP_PORT === 443 ? "" : `:${process.env.APP_PORT}`}${process.env.API_V1}/${requestId}`
+                                    }
+                                });
+                        })
+                        .catch((error) => {
+                            res.status(500).send({
+                                status: 500,
+                                message: error.message
+                            });
+                            console.error("File upload was successful, but failed to log to the database.", "\nError:", error, "\nFile:", {newFilename: newFilename, fileExt: fileExt, filePath: filePath, fileMime: fileMime, requestId: requestId});
+                        });
+                });
+            } catch (error) {
+                console.log(error);
+                res.status(500).send({
+                    status: 500,
+                    message: error.message
+                });
+            }
+        });
+    }
+
+    GetRoutes() {
+        return (() => {
+            var router = require("express").Router();
+
+            router.post("/", this.uploadFile);
+
+            return router;
+        })();
+    }
+}
+
+module.exports = FileUpload;
