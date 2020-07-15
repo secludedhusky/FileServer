326485fcead016334201940d6697eb71a6b71331
XenoWarrior <admin@projectge.com>
2020-07-15 23:23:43 +0100

    Add logo to homepage

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,6 +1,11 @@
 <template>
     <v-container>
         <h1>Home</h1>
+
+        <v-row justify="center" cols="12" sm="8">
+            <v-img contain height="300px" width="154px" src="./assets/logo.png"></v-img>
+        </v-row>
+
         <home-form></home-form>
     </v-container>
 </template>
@@ -11,7 +16,7 @@ import HomeForm from "../forms/HomeForm.vue";
 export default {
     name: "home",
     components: {
-        HomeForm,
+        HomeForm
     }
 };
 </script>
